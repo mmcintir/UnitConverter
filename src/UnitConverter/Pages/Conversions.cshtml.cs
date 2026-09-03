d70faadd4fc8ed22bbbb0ebc8f1e6eb97e75e597
matthewmcintire <mmcintir@wvup.edu>
2026-09-03 15:28:51 -0400

    Lesson 1 Tests

--- src/UnitConverter/Pages/Conversions.cshtml.cs
+++ src/UnitConverter/Pages/Conversions.cshtml.cs
@@ -1,11 +1,21 @@
+using System.Globalization;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace UnitConverter.Pages;
 
-public class Conversions : PageModel
+public class ConversionsModel : PageModel
 {
+    public string Input { get; set; } = "3.1415";
+    public string Output { get; set; } = string.Empty;
+
     public void OnGet()
     {
-        
+        Input = "3.1415";
+        ViewData["ConversionType"] = "Miles to Kilometers";
+        ViewData["Title"] = "Conversions";
+        double miles = Convert.ToDouble(Input);
+        UnitOf.Length conversion = new UnitOf.Length().FromMiles(miles);
+        Output = Convert.ToString(conversion.ToKilometers(), CultureInfo.InvariantCulture);
     }
 }
